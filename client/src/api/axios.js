6e9d9da2a8024b0d6f1b52b3b8ca734d8df942f3
Aditya Atbhaiya <126330557+AdityaAtb09@users.noreply.github.com>
2023-09-25 12:30:24 +0530

    Update axios.js

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -3,7 +3,8 @@ import axios from "axios";
 const port = process.env.PORT || 8080;
 
 export default axios.create({
-  baseURL: "https://library-management-fawn.vercel.app/api",
+  // baseURL: "https://library-management-fawn.vercel.app/api",
+  baseURL: "https://backend-orvm.onrender.com/api"
   //   baseURL: port,
 
   headers: {
